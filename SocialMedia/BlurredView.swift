cf4101153ba329ae2256ae431dd691fda29bca9b
Benjamin Lee <benjlee92@gmail.com>
2017-05-27 11:54:39 -0700

    Added SwiftKeychainWrapper

---
+++ SocialMedia/BlurredView.swift
@@ -0,0 +1,21 @@
+//
+//  BlurredView.swift
+//  SocialMedia
+//
+//  Created by Ben on 5/23/17.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import UIKit
+
+class BlurredView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Ben on 5/27/17.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import Foundation
