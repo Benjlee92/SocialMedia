7431058007644ea235685211ceaa6954b30e0de0
Benjamin Lee <benjlee92@gmail.com>
2017-05-27 13:40:29 -0700

    added segue/tableviews and tabelView UI

---
+++ SocialMedia/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  SocialMedia
+//
+//  Created by Ben on 5/27/17.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
